8ef8d7cfd75f509d4d57af15b1e82763d98bc32a
dylansong <dylan.song@gmail.com>
2023-05-08 11:40:44 -0700

    230508114043

--- clear_deployments.go
+++ clear_deployments.go
@@ -5,48 +5,21 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"sort"
 	"time"
 )
 
-func (uploader *CFPagesUploader) GetAllDeployments() []byte {
-	// get all deployments (GET https://api.cloudflare.com/client/v4/accounts/{account_id}/pages/projects/{project_name}/deployments)
+func (cf *CFPagesUploader) GetAllDeployments() ([]Deployment, error) {
+	// get all deployments (GET https://api.cloudflare.com/client/v4/accounts/887ebfb71fdc5f2a59d20ab48eb4c9b2/pages/projects/kite-cms/deployments)
 
 	// Create client
 	client := &http.Client{}
 
 	// Create request
-	url := fmt.Sprintf("%s/accounts/%s/pages/projects/%s/deployments", uploader.BaseURL, uploader.AccountID, uploader.ProjectName)
-	req, err := http.NewRequest("GET", url, nil)
+	reqURL := fmt.Sprintf("%s/accounts/%s/pages/projects/%s/deployments", cf.BaseURL, cf.AccountID, cf.ProjectName)
+	req, err := http.NewRequest("GET", reqURL, nil)
 
 	// Headers
-	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", uploader.APIKey))
-
-	// Fetch Request
-	resp, err := client.Do(req)
-
-	if err != nil {
-		fmt.Println("Failure : ", err)
-	}
-
-	// Read Response Body
-	respBody, _ := io.ReadAll(resp.Body)
-
-	return respBody
-}
-
-func (uploader *CFPagesUploader) DeleteDeployment(deploymentID string) {
-	// delete deployment (DELETE https://api.cloudflare.com/client/v4/accounts/{account_id}/pages/projects/{project_name}/deployments/{deployment_id})
-
-	// Create client
-	client := &http.Client{}
-
-	// Create request
-	url := fmt.Sprintf("%s/accounts/%s/pages/projects/%s/deployments/%s", uploader.BaseURL, uploader.AccountID, uploader.ProjectName, deploymentID)
-	req, err := http.NewRequest("DELETE", url, nil)
-
-	// Headers
-	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", uploader.APIKey))
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", cf.APIKey))
 
 	// Fetch Request
 	resp, err := client.Do(req)
@@ -62,44 +35,97 @@ func (uploader *CFPagesUploader) DeleteDeployment(deploymentID string) {
 	fmt.Println("response Status : ", resp.Status)
 	fmt.Println("response Headers : ", resp.Header)
 	fmt.Println("response Body : ", string(respBody))
+
+	var deploymentsResp DeploymentsResponse
+	err = json.Unmarshal(respBody, &deploymentsResp)
+	if err != nil {
+		return nil, err
+	}
+
+	return deploymentsResp.Result, nil
+}
+
+func (cf *CFPagesUploader) DeleteDeployment(deploymentID string) error {
+	// delete deployment (DELETE https://api.cloudflare.com/client/v4/accounts/887ebfb71fdc5f2a59d20ab48eb4c9b2/pages/projects/kite-cms/deployments/1dd9af2c-7c51-4a32-b2cf-b15c9f2728d1)
+
+	// Create client
+	client := &http.Client{}
+
+	// Create request
+	reqURL := fmt.Sprintf("%s/accounts/%s/pages/projects/%s/deployments/%s", cf.BaseURL, cf.AccountID, cf.ProjectName, deploymentID)
+	req, err := http.NewRequest("DELETE", reqURL, nil)
+
+	// Headers
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", cf.APIKey))
+
+	// Fetch Request
+	resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println("Failure : ", err)
+	}
+
+	// Read Response Body
+	respBody, _ := io.ReadAll(resp.Body)
+
+	// Display Results
+	fmt.Println("response Status : ", resp.Status)
+	fmt.Println("response Headers : ", resp.Header)
+	fmt.Println("response Body : ", string(respBody))
+	// Handle errors
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to delete deployment: status %s, body %s", resp.Status, string(respBody))
+	}
+
+	return nil
 }
 
 type Deployment struct {
 	ID         string    `json:"id"`
-	URL        string    `json:"url"`
-	State      string    `json:"state"`
-	CreatedOn  time.Time `json:"created_on"`
 	ModifiedOn time.Time `json:"modified_on"`
 }
 
-type Deployments []Deployment
-
-func (slice Deployments) Len() int {
-	return len(slice)
+type DeploymentsResponse struct {
+	Success    bool `json:"success"`
+	ResultInfo struct {
+		Page       int `json:"page"`
+		PerPage    int `json:"per_page"`
+		Count      int `json:"count"`
+		TotalCount int `json:"total_count"`
+	} `json:"result_info"`
+	Result []Deployment `json:"result"`
 }
 
-func (slice Deployments) Less(i, j int) bool {
-	return slice[i].ModifiedOn.Before(slice[j].ModifiedOn)
-}
-
-func (slice Deployments) Swap(i, j int) {
-	slice[i], slice[j] = slice[j], slice[i]
-}
-
-func (uploader *CFPagesUploader) DeleteAllDeploymentsExceptLatest() {
-	// Get all deployments
-	respBody := uploader.GetAllDeployments()
-	var deployments Deployments
-	json.Unmarshal(respBody, &deployments)
-
-	// Sort deployments by modified_on
-	sort.Sort(deployments)
-
-	// Delete all deployments except the latest one
-	latestDeployment := deployments[len(deployments)-1]
-	for _, deployment := range deployments[:len(deployments)-1] {
-		uploader.DeleteDeployment(deployment.ID)
+func (cf *CFPagesUploader) DeleteAllButLatestDeployment() {
+	deployments, err := cf.GetAllDeployments()
+	if err != nil {
+		fmt.Println("Error getting all deployments:", err)
+		return
 	}
 
-	fmt.Printf("Deleted %d deployments. Latest deployment: %s\n", len(deployments)-1, latestDeployment.ID)
+	if len(deployments) <= 1 {
+		return
+	}
+
+	latest := deployments[0]
+	for _, deployment := range deployments {
+		if deployment.ModifiedOn.After(latest.ModifiedOn) {
+			latest = deployment
+		}
+	}
+
+	for _, deployment := range deployments {
+		if deployment.ID != latest.ID {
+			err := cf.DeleteDeployment(deployment.ID)
+			if err != nil {
+				fmt.Println("Error deleting deployment:", err)
+			} else {
+				fmt.Printf("Deleted deployment %s\n", deployment.ID)
+			}
+		}
+	}
 }
